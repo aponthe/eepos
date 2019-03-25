2602448c544337a8bed22e8156479e3b705afc76
aponthe <46538170+aponthe@users.noreply.github.com>
2019-03-25 00:14:02 -0600

    summary

---
+++ js/resolucion.js
@@ -0,0 +1,6 @@
+var anchuraventana = window.innerWidth;
+var alturaventana = window.innerHeight;
+
+alert("ANCHURA MAXIMA PARA EL MONITOR: "+window.screen.availWidth + " ALTURA MAXIMA PARA EL MONITOR: "+window.screen.availHeight);
+
+alert("ANCHURA VENTANA: "+anchuraventana + " ALTURA VENTANA: "+ alturaventana);
